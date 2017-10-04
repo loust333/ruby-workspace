2ab2498a37fd0a9f0ba6e1ba07783091c20f1795
Steve Lourenco <lourenco.steve@outlook.com>
2017-10-04 18:31:42 +0200

    Find the extension and read

--- OOP_files/file_operator.rb
+++ OOP_files/file_operator.rb
@@ -6,4 +6,20 @@ require_relative 'json_reader'
 # FileReader.new(ARGV[0]).read
 # CsvReader.new(ARGV[0]).read
 # YamlReader.new(ARGV[0]).read
-JsonReader.new(ARGV[0]).read
+# JsonReader.new(ARGV[0]).read
+
+FILENAME = ARGV[0]
+
+def file_extension(file_name)
+  file_name.split('.').last
+end
+
+def reader_class
+  ext = file_extension(FILENAME)
+  return CsvReader if ext == 'csv'
+  return YamlReader if ext == 'yml'
+  return JsonReader if ext == 'json'
+  FileReader
+end
+
+reader_class.new(FILENAME).read
