e0b651abdf0d7bea7aa1d4c9c8f86722de749846
Steve Lourenco <lourenco.steve@outlook.com>
2017-10-17 21:07:47 +0200

    Added historical stock quotes saver, couldn’t t use yahoo finance because of the end of the gem historical quotes function. Replaced using quotes gem

--- finance_project/historical_stock_quote.rb
+++ finance_project/historical_stock_quote.rb
@@ -1,7 +1,32 @@
-require 'yahoo-finance'
+# require 'yahoo-finance'
+require 'yquotes'
 
-yahoo_client = YahooFinance::Client.new
+class HistoricalStockQuote
+  def self.historical_price(symbol)
+    yahoo_client = YQuotes::Client.new
 
-data = yahoo_client.historical_quotes('AAPL', start_date: Time.now - (24 * 60 * 60 * 365), end_date: Time.now)
+    quotes =
+      yahoo_client.get_quote(
+        symbol,
+        {
+          period: 'd',
+          start_date: (Date.today - 365).strftime('%Y-%m-%d'),
+          end_date: Date.today.strftime('%Y-%m-%d')
+        }
+      )
 
-puts data
+    quotes.map_rows do |quote|
+      [quote.date, quote.open]
+    end
+  end
+
+  def self.write_price_by_symbol(symbol)
+    quotes = historical_price(symbol)
+    filename = "#{symbol}.csv"
+    CSV.open(filename, 'w') do |file|
+      quotes.each do |quote|
+        file << quote
+      end
+    end
+  end
+end
--- finance_project/runner.rb
+++ finance_project/runner.rb
@@ -1,11 +1,15 @@
-require_relative 'stock_quote'
+# require_relative 'stock_quote'
+require_relative 'historical_stock_quote'
 # Runner to execute finance_project application
+#
+# def stock_price_selector(*symbol_array)
+#   symbol_array.each do |symbol|
+#     stock_price = StockQuote.price_by_symbol(symbol)
+#     StockQuote.write_price_by_symbol(symbol, stock_price)
+#   end
+# end
+#
+# stock_price_selector('AAPL', 'GOOG', 'MSFT')
 
-def stock_price_selector(*symbol_array)
-  symbol_array.each do |symbol|
-    stock_price = StockQuote.price_by_symbol(symbol)
-    StockQuote.write_price_by_symbol(symbol, stock_price)
-  end
-end
-
-stock_price_selector('AAPL', 'GOOG', 'MSFT')
+puts HistoricalStockQuote.historical_price('MSFT')
+HistoricalStockQuote.write_price_by_symbol('MSFT')
